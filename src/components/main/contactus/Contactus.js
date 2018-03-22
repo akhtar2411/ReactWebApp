18033e691c57b9e8bf29459d2f8800a777f458fd
Akhtar H Sayyed <akhtar.sayyed@edmodo.com>
2018-03-22 20:38:45 +0530

    jsx-expression-nmwm: Created few functions

--- src/components/main/contactus/Contactus.js
+++ src/components/main/contactus/Contactus.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
+function Repeat(props) {
+  let items = [];
+  for (let i = 0; i < props.numTimes; i++) {
+    items.push(props.children(i));
+  }
+  return <div>{items}</div>;
+}
+
+function ListOfTenThings() {
+  return (
+    <Repeat numTimes={10}>
+      {index => <div key={index}>This is item {index} in the list</div>}
+    </Repeat>
+  );
+}
+
 class Contactus extends React.Component {
   constructor() {
     super();
@@ -9,6 +25,7 @@ class Contactus extends React.Component {
     return (
       <div>
         <p> {this.state.contactus}</p>
+        <ListOfTenThings />
       </div>
     );
   }
--- src/components/main/home/Home.js
+++ src/components/main/home/Home.js
@@ -1,5 +1,71 @@
 import React from 'react';
+import { Header } from '../header/Header';
+import { Footer } from '../footer/Footer';
 
+function FancyBorder(props) {
+  return (
+    <div className={'FancyBorder FancyBorder-' + props.color}>
+      {props.children}
+    </div>
+  );
+}
+
+function WelcomeDialog() {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">Welcome</h1>
+      <p className="Dialog-message">Thank you for visiting our spacecraft!</p>
+    </FancyBorder>
+  );
+}
+
+function SplitPane(props) {
+  return (
+    <div className="SplitPane">
+      <div className="SplitPane-left">{props.left}</div>
+      <div>{props.right}</div>
+    </div>
+  );
+}
+
+function App() {
+  return <SplitPane left={<Header />} right={<Footer />} />;
+}
+function UserDialog(props) {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">{props.title}</h1>
+      <p className="Dialog-message">{props.message}</p>
+      {props.children}
+    </FancyBorder>
+  );
+}
+
+class SignUpDialog extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.state = { login: '' };
+  }
+  render() {
+    return (
+      <UserDialog
+        title="Mars Exploration Program"
+        message="How should we refer to you?"
+      >
+        <input value={this.state.login} onChange={this.handleChange} />
+        <button onClick={this.handleSignUp}>Sign Me up!</button>
+      </UserDialog>
+    );
+  }
+  handleChange(e) {
+    this.setState({ login: e.target.value });
+  }
+  handleSignUp() {
+    alert(`Welcome aboard, ${this.state.login}`);
+  }
+}
 export class Home extends React.Component {
   constructor() {
     super();
@@ -9,6 +75,9 @@ export class Home extends React.Component {
     return (
       <div>
         <p>{this.state.home}</p>
+        <WelcomeDialog />
+        <App />
+        <SignUpDialog />
       </div>
     );
   }
